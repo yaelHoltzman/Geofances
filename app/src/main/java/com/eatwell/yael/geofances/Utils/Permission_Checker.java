63a1406f9109a9bcfd4f4f3f8e06f9321ba1da40
yaelHoltzman <yael.eatwell@gmail.com>
2018-10-14 02:37:26 +0300

    Wallpaper bug fixed

--- app/src/main/java/com/eatwell/yael/geofances/Utils/Permission_Checker.java
+++ app/src/main/java/com/eatwell/yael/geofances/Utils/Permission_Checker.java
@@ -10,7 +10,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class Permission_Checker extends AppCompatActivity {
+public class Permission_Checker {
     private static final String TAG = Permission_Checker.class.getSimpleName();
 
     private static final int REQ_PERMISSION = 999;
